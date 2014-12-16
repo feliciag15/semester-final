be1bbe362a608135e9229ee23b6c338f015254fd
feliciag15 <felicia_grable@asl.org>
2014-12-16 15:23:07 +0000

    Update TestChallenge3.java

--- src/TestChallenge3.java
+++ src/TestChallenge3.java
@@ -1,3 +1,5 @@
+import edu.fcps.karel2.Display;
+
 /**
  * Read and implement all of the TODO items in the main method, below.
  * 
@@ -6,10 +8,16 @@ public class TestChallenge3 {
 	// TODO Complete the BeepFarmer class, provided, by implementing the reap() method
 
 	public static void main(String[] args) {
-		// TODO Load the "farm" map at set its size to 12 by 12
+		
+		Display.openWorld("src/maps/farm.map");
+		Display.setSize(12, 12);
+		
+		BeepFarmer paul = new BeepFarmer();
+		paul.reap(3, 6);
 		// TODO Create an instance of a BeepFarmer with no beepers, anywhere on the farm
 		// TODO Have the farmer gather all the beepers from the garden plot in the center of the map
 		// TODO Have the farmer drop at location (1, 1) all the beepers picked and then move aside.
 	}
 
 }
+

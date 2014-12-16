f4d11e241a30140bc93e426693d9a3c197aecf53
feliciag15 <felicia_grable@asl.org>
2014-12-16 15:21:36 +0000

    Update Horse.java

--- src/Horse.java
+++ src/Horse.java
@@ -1,5 +1,5 @@
 import org.asl.karelx.Wanderer;
-
+import edu.fcps.karel2.Display;
 /**
  * Represents a horse wandering around aimlessly in a pasture
  *
@@ -7,6 +7,16 @@ import org.asl.karelx.Wanderer;
 public class Horse extends Wanderer {
 
 	//  TODO Add one or more constructors, as needed
+	
+	public Horse() {
+		
+		super (1, 1);
+	}
+	
+	public Horse(int x, int y) {
+		
+		super (x, y);
+	}
 	
 	/**
 	 * Wander around in random directions until a specified number of steps have been taken.
@@ -19,5 +29,35 @@ public class Horse extends Wanderer {
 	 */
 	public void wander(int count, int timer) {
 		// TODO You implement this
+		
+		if (!backIsClear()) {
+			
+			move();
+		} else if (!leftIsClear()) {
+			
+			turnRight();
+			
+		} else if (!rightIsClear()) {
+			
+			turnLeft();
+			
+		}
+		
+		for (int i = 0; i <= count; i++) {
+			
+			wander(count);
+			
+			if (i == timer) {
+				
+				putBeeper();
+			} else if (i == (timer + timer))
+			
+				putBeeper();
+		
+		}
+		
+		
+		
+		
 	}
 }
